00079734509793e9aa239e06ef575ffd041e8433
SoloDragon <123315158+Astrxgore@users.noreply.github.com>
2025-03-21 03:20:38 +0500

    Add files via upload

---
+++ values.js
@@ -0,0 +1,31 @@
+// values.js
+const cableOptions = [
+    {
+      name: "Изолированный",
+      outageFrequencyForest: 0.02,
+      outageFrequencyNonForest: 0.01,
+      costNonForest: 500000,
+      costForest: 550000
+    },
+    {
+      name: "Неизолированный",
+      outageFrequencyForest: 0.1,
+      outageFrequencyNonForest: 0.05,
+      costNonForest: 400000,
+      costForest: 450000
+    }
+  ];
+  
+
+const equipmentCostsFromFile = {
+    "Реклоузер": 250000,
+    "Выключатель": 250000,
+    "Разъединитель": 20000
+};
+  
+
+  // Ущерб от отключения (руб./час) для каждого города (ID 2-15)
+const cityOutageCosts = {
+    2: 50000, 3: 50000, 4: 50000, 5: 100000, 6: 50000, 7: 200000, 8: 50000,
+    9: 200000, 10: 100000, 11: 100000, 12: 50000, 13: 50000, 14: 50000, 15: 100000
+};
